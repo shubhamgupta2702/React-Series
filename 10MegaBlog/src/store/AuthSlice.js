9c50cde223fc36a0f73f3457225a06c71eaac2e7
Shubham <shubhamgupta43567@gmail.com>
2024-10-02 01:41:02 +0530

    react OP

--- 10MegaBlog/src/store/AuthSlice.js
+++ 10MegaBlog/src/store/AuthSlice.js
@@ -14,6 +14,10 @@ const authSlice = createSlice({
             state.status = true;
             state.userData = action.payload.userData;
         },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
   }
 }
 )
